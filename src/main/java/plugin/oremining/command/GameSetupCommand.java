33caa6c8e69f7b3148b6625378ea8aa2abebbd9c
t-suzuki-code <tomotomoj0914@icloud.com>
2026-03-07 23:09:22 +0900

    テレポート用のポータルを生成

--- src/main/java/plugin/oremining/command/GameSetupCommand.java
+++ src/main/java/plugin/oremining/command/GameSetupCommand.java
@@ -1,5 +1,6 @@
 package plugin.oremining.command;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -18,10 +19,12 @@ public class GameSetupCommand implements CommandExecutor {
     if (sender instanceof Player player) {
 
       resetPlayerStatus(player);
+      generatePortal(player);
 
     }
-    return false;
+    return true;
   }
+
 
   /**
    * ゲームを始める前にプレイヤーの状態を設定する。 体力・空腹値・状態異常を初期化 装備をネザライト一式にし、インベントリに火打石を追加。
@@ -54,6 +57,42 @@ public class GameSetupCommand implements CommandExecutor {
     player.getActivePotionEffects().stream()
         .map(PotionEffect::getType)
         .forEach(player::removePotionEffect);
+  }
+
+  /**
+   * ポータル生成のための黒曜石を配置します。
+   *
+   * @param location 黒曜石を配置するロケーション。
+   */
+  private void placeObsidian(Location location) {
+    location.getBlock().setType(Material.OBSIDIAN);
+  }
+
+  /**
+   * テレポートするためのポータルを作ります
+   *
+   * @param player コマンドを実行したプレイヤー
+   */
+  private void generatePortal(Player player) {
+    Location playerLocation = player.getLocation();
+    double z = playerLocation.getZ() + 5;
+
+    for (int i = 0; i < 4; i++) {
+
+      double x = playerLocation.getX() + i;
+
+      placeObsidian(new Location(player.getWorld(), x, playerLocation.getY(), z));
+      placeObsidian(new Location(player.getWorld(), x, playerLocation.getY() + 4, z));
+
+    }
+    for (int i = 0; i < 5; i++) {
+
+      double y = playerLocation.getY() + i;
+
+      placeObsidian(new Location(player.getWorld(), playerLocation.getX(), y, z));
+      placeObsidian(new Location(player.getWorld(), playerLocation.getX() + 3, y, z));
+
+    }
   }
 }
 
