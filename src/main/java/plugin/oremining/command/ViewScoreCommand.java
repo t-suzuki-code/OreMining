a21b35f2bcf1fdc8bb780f8a1ea85142e7b4f209
t-suzuki-code <tomotomoj0914@icloud.com>
2026-03-19 20:27:23 +0900

    BaseCommandの追加

--- src/main/java/plugin/oremining/command/ViewScoreCommand.java
+++ src/main/java/plugin/oremining/command/ViewScoreCommand.java
@@ -3,31 +3,37 @@ package plugin.oremining.command;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jspecify.annotations.NonNull;
 import plugin.oremining.DBManager;
 import plugin.oremining.mapper.data.PlayerScore;
 
-public class ViewScoreCommand implements CommandExecutor {
+public class ViewScoreCommand extends BaseCommand {
+
 
   @Override
-  public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command,
-      @NonNull String label, @NonNull String @NonNull [] args) {
-    if (sender instanceof Player player) {
-      DBManager dbManager = new DBManager();
-      List<PlayerScore> playerScoreList = dbManager.selectList();
+  public boolean onExecutePlayerCommand(Player player, Command command, String label,
+      String[] args) {
 
-      for (PlayerScore playerScore : playerScoreList) {
-        player.sendMessage(
-            playerScore.getId() + " | "
-                + playerScore.getPlayerName() + " | "
-                + playerScore.getScore() + " | "
-                + playerScore.getRegisteredAt()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-      }
+    DBManager dbManager = new DBManager();
+    List<PlayerScore> playerScoreList = dbManager.selectList();
+
+    for (PlayerScore playerScore : playerScoreList) {
+      player.sendMessage(
+          playerScore.getId() + " | "
+              + playerScore.getPlayerName() + " | "
+              + playerScore.getScore() + " | "
+              + playerScore.getRegisteredAt()
+              .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
+
     return true;
   }
+
+  @Override
+  public boolean onExecuteConsoleCommand(CommandSender sender, Command command, String label,
+      String[] args) {
+    return false;
+  }
 }
+
