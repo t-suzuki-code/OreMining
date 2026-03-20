4d0d0661630e724d94a70229056b9b72fc8e5fe7
t-suzuki-code <tomotomoj0914@icloud.com>
2026-03-20 10:37:11 +0900

    コマンドの連打防止とゲーム準備状態に制限時間を追加

--- src/main/java/plugin/oremining/command/GameSetupCommand.java
+++ src/main/java/plugin/oremining/command/GameSetupCommand.java
@@ -3,15 +3,45 @@ package plugin.oremining.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import plugin.oremining.Main;
 import plugin.oremining.PlayerUtils;
 
 public class GameSetupCommand extends BaseCommand {
 
+  private final Main main;
+  private boolean isGameReady = false;
+  private BukkitTask task;
+
+  public GameSetupCommand(Main main) {
+    this.main = main;
+  }
+
+
   @Override
   public boolean onExecutePlayerCommand(Player player, Command command, String label,
       String[] args) {
-    PlayerUtils.resetPlayerStatus(player);
-    PlayerUtils.generatePortal(player);
+    if (!isGameReady) {
+
+      isGameReady = true;
+      PlayerUtils.resetPlayerStatus(player);
+      PlayerUtils.generatePortal(player);
+      player.sendMessage("ゲーム準備状態になりました。ポータルに入りゲームを開始してください！");
+      player.sendMessage("ポータルに入らなかった場合、ゲーム準備状態は30秒で終了します。");
+
+      task = new BukkitRunnable() {
+        @Override
+        public void run() {
+          player.sendMessage("30秒経過したため、ゲーム準備状態を終了しました。");
+          resetGameSetup();
+          PlayerUtils.removePortal();
+        }
+      }.runTaskLater(main, 20 * 30);
+
+    } else {
+      player.sendMessage("ゲーム準備状態のため、コマンドを実行できません。");
+    }
     return true;
   }
 
@@ -19,6 +49,11 @@ public class GameSetupCommand extends BaseCommand {
   public boolean onExecuteConsoleCommand(CommandSender sender, Command command, String label,
       String[] args) {
     return false;
+  }
+
+  public void resetGameSetup() {
+    task.cancel();
+    isGameReady = false;
   }
 
 
