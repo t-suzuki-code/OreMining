62bfcb86ccb479d1d06852264e33c74d4cf7a47b
t-suzuki-code <tomotomoj0914@icloud.com>
2026-04-05 14:41:18 +0900

    対象パッケージのリファクタリング

--- src/main/java/plugin/oremining/listener/PlayerScoreListener.java
+++ src/main/java/plugin/oremining/listener/PlayerScoreListener.java
@@ -13,10 +13,9 @@ import plugin.oremining.GameState;
 
 public class PlayerScoreListener implements Listener {
 
-
-  private int count;
+  private int comboCount;
   private Material oreType;
-
+  private final int BONUS_REQUIRED_COMBO = 3;
   @Getter
   private int playerScore;
   @Setter
@@ -32,38 +31,61 @@ public class PlayerScoreListener implements Listener {
 
       switch (e.getBlock().getType()) {
         case DIAMOND_ORE, GOLD_ORE, IRON_ORE -> {
-
-          if (oreType == e.getBlock().getType()) {
-            count = count + 1;
-          } else {
-            count = 1;
-          }
-
-          if (count == 3) {
-            playerScore = playerScore + 30;
-            count = 0;
-            player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "★ ボーナス！+30点 ★");
-          }
-
+          updateComboCount(e);
+          addBonusScore(player);
           oreType = e.getBlock().getType();
-
-          switch (oreType) {
-            case DIAMOND_ORE -> {
-              playerScore = playerScore + 30;
-              player.sendMessage(ChatColor.AQUA + "[+30] ダイヤモンド鉱石を採掘！");
-            }
-            case GOLD_ORE -> {
-              playerScore = playerScore + 20;
-              player.sendMessage(ChatColor.GOLD + "[+20] 金鉱石を採掘！");
-            }
-            case IRON_ORE -> {
-              playerScore = playerScore + 10;
-              player.sendMessage(ChatColor.GRAY + "[+10] 鉄鉱石を採掘！");
-            }
-            default -> {
-            }
-          }
+          addScore(player);
         }
+      }
+    }
+  }
+
+  /**
+   * 同じ種類の鉱石の連続採掘回数をカウントします。
+   *
+   * @param e イベント情報。
+   */
+  private void updateComboCount(BlockBreakEvent e) {
+    if (oreType == e.getBlock().getType()) {
+      comboCount = comboCount + 1;
+    } else {
+      comboCount = 1;
+    }
+  }
+
+  /**
+   * 同じ種類の鉱石が連続で規定回数採掘されたときボーナス点の処理を行います。
+   *
+   * @param player ゲーム中のプレイヤー
+   */
+  private void addBonusScore(Player player) {
+    if (comboCount == BONUS_REQUIRED_COMBO) {
+      playerScore = playerScore + 30;
+      comboCount = 0;
+      player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "★ ボーナス！+30点 ★");
+    }
+  }
+
+  /**
+   * 採掘した鉱石の種類によってスコア加算を行います。
+   *
+   * @param player ゲーム中のプレイヤー
+   */
+  private void addScore(Player player) {
+    switch (oreType) {
+      case DIAMOND_ORE -> {
+        playerScore = playerScore + 30;
+        player.sendMessage(ChatColor.AQUA + "[+30] ダイヤモンド鉱石を採掘！");
+      }
+      case GOLD_ORE -> {
+        playerScore = playerScore + 20;
+        player.sendMessage(ChatColor.GOLD + "[+20] 金鉱石を採掘！");
+      }
+      case IRON_ORE -> {
+        playerScore = playerScore + 10;
+        player.sendMessage(ChatColor.GRAY + "[+10] 鉄鉱石を採掘！");
+      }
+      default -> {
       }
     }
   }
@@ -73,7 +95,7 @@ public class PlayerScoreListener implements Listener {
    */
   public void resetPlayerScore() {
     playerScore = 0;
-    count = 0;
+    comboCount = 0;
     oreType = null;
   }
 }
